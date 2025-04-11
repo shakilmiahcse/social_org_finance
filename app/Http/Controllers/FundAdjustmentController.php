4f79d7ae37c624a00f1ef4594b7f34c73175eb07
Shakil Miah <shakilmiahcse36@gmail.com>
2025-04-11 16:50:37 +0600

    fund adjustment

--- app/Http/Controllers/FundAdjustmentController.php
+++ app/Http/Controllers/FundAdjustmentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TransactionAdjustmentController extends Controller
+class FundAdjustmentController extends Controller
 {
     /**
      * Display a listing of the resource.
