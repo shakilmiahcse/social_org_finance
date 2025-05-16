b638271a5e2aaa83da5dd2452cc28caf16fed08f
Shakil Miah <shakilmiahcse36@gmail.com>
2025-05-16 09:45:27 +0600

    welcome page done

---
+++ resources/js/components/CopyRight.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="w-full text-sm text-gray-500">
+        <div class="border-t border-gray-800 mt-12 pt-5 flex flex-col md:flex-row justify-between items-center">
+            <div class="flex flex-wrap gap-x-2">
+                <span>© Oikko Soft {{ currentYear }}. All rights reserved.</span>
+                <span>|</span>
+                <span>
+                    Developed by
+                    <a href="#" class="hover:underline text-success">Shakil Miah</a>
+                </span>
+            </div>
+            <div class="text-right">
+                Powered by Rokeya Begum Foundation
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
